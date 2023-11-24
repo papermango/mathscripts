8e47ed0d000b58fcc281721a4d409cd894ec4a2a
Parth Deshmukh <parthpd24@gmail.com>
2023-11-24 00:03:06 -0600

    Implement stdinput for CTS
    stdin input has been implemented, and the same code should work to implement file input. The last things remaining are to fix up a few things: correcting repeat-finder to tell identical-looking productions in different orders apart and fixing short machine printout to print last nonempty state. Additional CLI editing is appreciated but optional.

--- Haskell/cyclictagsystem.hs
+++ Haskell/cyclictagsystem.hs
@@ -1,4 +1,7 @@
 import Options.Applicative
+import Text.Read (readMaybe)
+
+--- Basic type definitions ---
 
 data Alphabet = Zero | One
     deriving (Show, Eq)
@@ -12,24 +15,25 @@ printer (Zero:xs) = "0" ++ printer xs
 printer (One:xs) = "1" ++ printer xs
 
 -- Encapsulates the state of the system at a snapshot
-data State = State [Main.Word] Main.Word Int | Halted Int | Empty
+data State = State [Main.Word] Main.Word Int
+            | Halted Int
+            | Empty
 
 -- Defines Show and Eq  on states
 -- Eq is a weaker equality which considers repeated states equal
+-- Possible way to find true repeats that accounts for duplicate words:
+    -- zip productions with indices and match on both of them
 instance Show State where
-    show :: State -> String
     show (State p w i) = printer w ++ " at step " ++ show i ++ ", next production " ++ printer (head p)
     show (Halted i) = "Halted at step " ++ show i
     show Empty = "N/A"
 instance Eq State where
-    (==) :: State -> State -> Bool
     (==) (State p1 w1 _) (State p2 w2 _) = (head p1 == head p2) && (w1 == w2)
-    (==) (State {}) (Halted _) = False
-    (==) (Halted _) (State {})  = False
-    (==) (Halted _) (Halted _) = True
-    (==) Empty Empty = True
-    (==) (Halted _) Empty = True
-    (==) Empty (Halted _) = True
+    (==) State {} _ = False
+    (==) _ State {}  = False
+    (==) _ _ = True
+
+--- Running the machine ---
 
 -- Logic to update the machine at each step
 update :: State -> State
@@ -37,10 +41,10 @@ update (State (p:productions) (Zero:xs) i) = State productions xs (i + 1)
 update (State (p:productions) (One:xs) i) = State productions (xs ++ p) (i + 1)
 update (State _ [] i) = Halted i
 update (State [] xs i) = Empty -- invalid state
-update (Halted i) = Halted i
+update (Halted i) = Empty
 update Empty = Empty -- invalid state
 
--- Runs the machine off steps and an initial state, allows specifying productions and starting word instead with runMachine --
+-- Runs the machine off steps and an initial state, allows specifying productions and starting word instead with runMachine
 runMachine :: Int -> State -> [State]
 runMachine n initstate = take (n + 1) (iterate update initstate)
 
@@ -50,7 +54,7 @@ runMachineFromInputs n productions word = runMachine n (State (cycle productions
 -- Pretty-print each state in sequence
 alignout :: State -> String
 alignout (State p w i) = replicate i ' ' ++ show (State p w i) ++ "\n"
-alignout (Halted i) = replicate (i + 1) ' ' ++ show (Halted i)
+alignout (Halted i) = replicate (i + 1) ' ' ++ show (Halted i) ++ "\n"
 alignout Empty = ""
 printoutputs :: [State] -> String
 printoutputs = concatMap alignout
@@ -61,25 +65,27 @@ findrepeatedstate :: [State] -> State
 findrepeatedstate [] = Empty
 findrepeatedstate (s:states) = if s `elem` states then s else findrepeatedstate states
 
--- Variables to set to run the machine. In the future, these will be enterable --
--- Production rules
-productions :: [Main.Word]
-productions = [[Zero, One, Zero],
+--- Default variables ---
+
+defProds :: [Main.Word]
+defProds = [[Zero, One, Zero],
                 [One, Zero],
                 [One, One],
                 []]
 
 -- Starting word
-word :: Main.Word
-word = [One, Zero, One, Zero, One]
+defWord :: Main.Word
+defWord = [One, Zero, One, Zero, One]
 
--- Parser code, using optparse-applicative package --
+--- Command line parser code, using optparse-applicative package ---
 
 -- Available options
 data Options = Options
     { verbose       :: Bool
     , stepsran      :: Int
-    , detectrepeats :: Bool}
+    , detectrepeats :: Bool
+    , inputIntMode  :: Bool
+    , inputData     :: Maybe Input}
 
 parseoptions :: Parser Options
 parseoptions = Options
@@ -89,7 +95,7 @@ parseoptions = Options
         <> help "Print the full machine runtime; otherwise, output end state only" )
     <*> option auto
         ( long "steps"
-        <> short 's'
+        <> short 'n'
         <> help "Steps count to run machine for"
         <> showDefault
         <> value 100
@@ -98,6 +104,11 @@ parseoptions = Options
         ( long "detectrepeats"
         <> short 'r'
         <> help "Look for a repeated state after running the machine")
+    <*> switch
+        ( long "integerinput"
+        <> short 'i'
+        <> help "Input initial machine state with unsigned integer encoding" )
+    <*> input -- Machine input parser, see below
 
 -- Wraps parser up with high-level help text and adds --help
 opts :: ParserInfo Options
@@ -106,27 +117,116 @@ opts = info (parseoptions <**> helper)
     <> progDesc "Simulate a cyclic tag system to a given number of steps. Halts are recognized only if the register clears. Optionally looks for repeating states after execution, which is the standard way to interpret halting in a cyclic tag system. Looking for repeats is quadratic in the number of steps run and linear in register length."
     <> header "ctshs - a cyclic tag system simulator written in Haskell")
 
--- Execution code --
+-- Parser for machine input --
+
+data Input
+  = FileInput FilePath
+  | StdInput
+
+fileInput :: Parser Input
+fileInput = FileInput <$> strOption
+  (  long "file"
+  <> short 'f'
+  <> metavar "FILENAME"
+  <> help "Read initial machine state from input file FILENAME" )
+
+stdInput :: Parser Input
+stdInput = flag' StdInput
+  (  long "stdin"
+  <> short 's'
+  <> help "Read initial machine state from stdin" )
+
+-- optional sets input to be optional
+input :: Parser (Maybe Input)
+input = optional $ fileInput <|> stdInput
+
+--- Read machine input ---
+
+-- Converts binary string into a Main.Word. Ignores symbols that are not 0 or 1
+readBinStr :: String -> Main.Word
+readBinStr "" = []
+readBinStr (x:xs)
+    | x == '0'  = Zero : readBinStr xs
+    | x == '1'  = One : readBinStr xs
+    | x == '-'  = []
+    | otherwise = readBinStr xs
+-- Reads off space-separated ints into a list of words
+readBinStrs :: String -> [Main.Word]
+readBinStrs = map readBinStr . words
+
+-- Converts integer into word
+readIntAsBin :: Int -> Main.Word
+readIntAsBin x
+    | x <= 1   = []
+    | rem == 0 = readIntAsBin div ++ [Zero]
+    | rem == 1 = readIntAsBin div ++ [One]
+    | otherwise = []
+    where (div, rem) = divMod x 2
+-- Reads off space-separated ints into a list of words
+readIntsAsBins :: String -> [Main.Word]
+readIntsAsBins = map (readIntAsBin . maybe 0 id)
+                    . filter (Nothing /=)
+                    . map (\x -> readMaybe x :: Maybe Int)
+                    . words
+
+--- Execution code ---
+
 main :: IO ()
 main = execute =<< execParser opts
 
+-- Main function to handle inputs
 execute :: Options -> IO ()
-execute (Options True n False) = do
-    putStrLn "Productions:"
-    print (map printer productions)
-    putStrLn "Output:"
-    putStr (printoutputs (runMachineFromInputs n productions word))
+-- No input; default execution
+execute opts@(Options _ _ _ _ Nothing) = executeOnInput opts defProds defWord
+-- File input
+execute opts@(Options _ _ _ True (Just (FileInput fp))) = print "Unimplemented"
+execute opts@(Options _ _ _ False (Just (FileInput fp))) =  print "Unimplemented"
+-- Stdin input
+execute opts@(Options _ _ _ True (Just StdInput)) = do
+    putStrLn "Enter starting word as an integer: "
+    wordstr <- getLine
+    putStrLn "Enter list of productions as integers, space-separated: "
+    prodstr <- getLine
+    putStrLn "---"
+    executeOnInput opts (readIntsAsBins prodstr) (head $ readIntsAsBins wordstr)
 
-execute (Options True n True) = do
-    putStrLn "Productions:"
-    print (map printer productions)
+execute opts@(Options _ _ _ False (Just StdInput)) = do
+    putStrLn "Enter starting word in binary: "
+    wordstr <- getLine
+    putStrLn "Enter list of productions in binary, space-separated: "
+    prodstr <- getLine
+    putStrLn "---"
+    executeOnInput opts (readBinStrs prodstr) (head $ readBinStrs wordstr)
+
+--- Print IO actions to stitch together program cmd output from ---
+
+printProductions :: [Main.Word] -> IO ()
+printProductions prod = putStr "Productions: " >> print (map printer prod) >> putStrLn ""
+
+-- Prints a found repeated state in a given machine
+printFoundRepeatedState :: [State] -> IO ()
+printFoundRepeatedState = (putStr "Repeated state at " >>) . print . findrepeatedstate
+
+-- Runs and prints the machine, including repeated state if asked for via True bool
+printShortMachine :: Int -> [Main.Word] -> Main.Word -> Bool -> IO ()
+printShortMachine steps productions word findrepeats = do
+    printProductions productions
+    print (head machineout)
+    putStrLn ""
+    print (last machineout) -- needs to be replaced with code that finds a halt if it happens and prints that
+    if findrepeats then printFoundRepeatedState machineout else putStr ""
+        where machineout = runMachineFromInputs steps productions word
+
+-- Runs and prints the machine verbosely, with same behavior as above
+printLongMachine :: Int -> [Main.Word] -> Main.Word -> Bool -> IO ()
+printLongMachine steps productions word findrepeats = do
+    printProductions productions
     putStrLn "Output:"
     putStrLn (printoutputs machineout)
-    putStr "Repeated state at "
-    print (findrepeatedstate machineout)
-        where machineout = runMachineFromInputs n productions word
+    if findrepeats then printFoundRepeatedState machineout else putStr ""
+        where machineout = runMachineFromInputs steps productions word
 
-execute (Options False n False) = print (last (runMachineFromInputs n productions word))
-
-execute (Options False n True) = print (last machineout) >> putStr "Repeated state at " >> print (findrepeatedstate machineout)
-    where machineout = runMachineFromInputs n productions word
+-- Handles non-input half of execute function
+executeOnInput :: Options -> [Main.Word] -> Main.Word -> IO ()
+executeOnInput (Options True n fr _ _) productions word = printLongMachine n productions word fr
+executeOnInput (Options False n fr _ _) productions word = printShortMachine n productions word fr
